dee47380331dedfdc0338b87fa92e2225cf335f6
wnewen <plpl541616@gmail.com>
2023-04-17 22:57:14 +0800

    manicure text align

--- src/components/Manicure/index.jsx
+++ src/components/Manicure/index.jsx
@@ -12,36 +12,6 @@ function Manicure() {
                 title2={title[1].catergory}
                 product="manicure"
             />
-            {/* <div className={styles.content}>
-                <div className={styles.lumiere}>
-                    <img src="./images/img_manicure_lumiere.svg" />
-                <NavLink to="/news">
-                <img src="./images/img_manicure_lumiere.svg"/>
-                </NavLink>
-                    <div className={styles.lumiere_text}>
-                        <p className={styles.text1}>Lumière</p>
-                        <div className={styles.p1}>
-                            <p className={styles.text2}>A Taiwan pressed-on nail brand</p>
-                            <p className={styles.text2}>100% handmade</p>
-                        </div>
-
-                    </div>
-
-                </div>
-                <div className={styles.ohora}>
-                    <div className={styles.ohora_text}>
-                        <p className={styles.text1}>ohora</p>
-                        <div className={styles.p1}>
-                            <p className={styles.text2}>A Korea nail brand</p>
-                            <p className={styles.text2}>Provided thousands of different nails </p>
-                        </div>
-
-                    </div>
-                    <img src="./images/img_manicure_ohora.svg" />
-                </div>
-
-
-            </div> */}
             <Row gutter={[32, 32]}>
                 <Col span={12} className={styles.left}>
                     <img src="./images/img_manicure_lumiere.svg" className={styles.img} />
